2bff3dd13c1aed34f5c0b7b22f33d737eb93c997
romanadamski <roman.adamski95@gmail.com>
2019-04-14 16:19:18 +0200

    Init commit

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlatformManager : MonoBehaviour {
+    public GameObject SinglePlatformElement;
+    public GameObject StoreForPlatforms;
+    public GameObject Player;
+    public TextMeshProUGUI SliderValue;
+    public Slider Slider;
+    public Button Generate;
+
+    float xBorder = 9;
+    float yBorder = 5.38f;
+    int Count;
+    float width;
+    float height;
+    float playerHeight;
+    System.Random random = new System.Random();
+    List<Transform> StoreForPlatformsChilds = new List<Transform>();
+    int maxHorizontalCount, maxVerticalCount;
+    // Use this for initialization
+    void Start () {
+        Generate = GameObject.Find("Generate").GetComponent<Button>();
+        Slider = GameObject.Find("Slider").GetComponent<Slider>();
+        SliderValue = GameObject.Find("SliderValue").GetComponent<TextMeshProUGUI>();
+
+        SliderValue.text = "" + Slider.value;
+        Slider.onValueChanged.AddListener(delegate { SliderValue.text = "" + Slider.value; });
+        Generate.onClick.AddListener(() => GenerateMap());
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+        playerHeight = Player.GetComponent<SpriteRenderer>().size.y * Player.transform.localScale.y;
+
+        width = SinglePlatformElement.GetComponent<SpriteRenderer>().size.x * SinglePlatformElement.transform.localScale.x;
+        height = SinglePlatformElement.GetComponent<SpriteRenderer>().size.y * SinglePlatformElement.transform.localScale.y;
+        xBorder -= width;
+        yBorder -= height;
+        maxHorizontalCount = (int)((xBorder * 2) / width);
+        maxVerticalCount = (int)((yBorder * 2) / (height + playerHeight));
+    }
+    void MakeNewPlatform(Vector2 position)
+    {
+        var newElement = Instantiate(SinglePlatformElement, position, new Quaternion(), StoreForPlatforms.transform);
+        StoreForPlatformsChilds.Add(newElement.transform);
+    }
+    void GenerateMap()
+    {
+        Count = (int)Slider.value;
+        if (Count > 0)
+        {
+            StoreForPlatformsChilds.Clear();
+            foreach (Transform go in StoreForPlatforms.GetComponentInChildren<Transform>())
+            {
+                Destroy(go.gameObject);
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                float x = GetRandomNumber(-xBorder, xBorder);
+                int countY = (int)GetRandomNumber(1, (maxVerticalCount + 1));
+                float y = countY * (height + playerHeight) - yBorder - playerHeight;
+                Debug.Log(y + ", " + (yBorder * 2) / y);
+                MakeNewPlatform(new Vector2(x, y));
+            }
+            SetPositionForCharacter();
+        }
+        
+    }
+    float GetRandomNumber(float min, float max)
+    {
+        if (min <= max)
+        {
+            var next = random.NextDouble();
+            return (float)(min + (next * (max - min)));
+        }
+        return 0;
+    }
+    void SetPositionForCharacter()
+    {
+        int index = (int)GetRandomNumber(0, Count);
+        Player.transform.SetPositionAndRotation(StoreForPlatformsChilds[index].position + new Vector3(0, height/2 + playerHeight/2, 0), new Quaternion());
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    Animator anim;
+    Rigidbody2D rb;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask WhatIsGround;
+    public float jumpHeight;
+    public float moveSpeed;
+    public bool grounded;
+    float jumpTimeCounter;
+    public float jumpTime;
+    bool jump;
+    bool longJump;
+    int jumpCounter = 0;
+
+    Quaternion turnRight;
+    Quaternion turnLeft;
+    float playerHeight;
+    float playerWidth;
+
+    float bottomBorder;
+    float topBorder;
+    float leftBorder;
+    float rightBorder;
+    void Start() {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        jump = false;
+        bottomBorder = -5.34f;
+        topBorder = 5.34f;
+        leftBorder = -9;
+        rightBorder = 9;
+        playerWidth = GetComponent<SpriteRenderer>().size.x * transform.localScale.x;
+        playerHeight = GetComponent<SpriteRenderer>().size.y * transform.localScale.y;
+
+        SetDirections();
+    }
+    void SetDirections()
+    {
+        turnRight = transform.rotation;
+        Vector3 rot = transform.rotation.eulerAngles;
+        rot = new Vector3(rot.x, rot.y + 180, rot.z);
+        turnLeft = Quaternion.Euler(rot);
+    }
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+        Destroy(GetComponent<PolygonCollider2D>());
+        gameObject.AddComponent<PolygonCollider2D>();
+        if (rb.velocity.magnitude < .01)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        FixPosition();
+    }
+    // Update is called once per frame
+    void Update() {
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("Grounded", grounded);
+        if (grounded)
+            jump = false;
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && grounded)
+        {
+            longJump = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = new Vector2(0, jumpHeight);
+            jumpCounter++;
+        }
+        if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) && longJump /*&& grounded*/)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = new Vector2(0, jumpHeight);
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                longJump = false;
+            }
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            transform.rotation = turnLeft;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            transform.rotation = turnRight;
+        }
+        if (!Input.anyKey)
+            rb.velocity = new Vector2(rb.velocity.x * 0.95f, rb.velocity.y);
+    }
+    void FixPosition()
+    {
+        if (transform.position.y <= bottomBorder)
+        {
+            transform.position = new Vector3(transform.position.x, topBorder, transform.position.z);
+            rb.velocity = new Vector2(0,1);
+        }
+        if (transform.position.x <= leftBorder)
+        {
+            transform.position = new Vector3(rightBorder, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x >= rightBorder)
+        {
+            transform.position = new Vector3(leftBorder, transform.position.y, transform.position.z);
+        }
+    }
+}
